b66a4aa6f14eb98d0c21fb2648b7b2bf9e1ed489
Scott <wsc@iri-labs.com>
2018-08-06 22:44:33 +0200

    add asif here resampling in dsp

---
+++ ops/asif.go
@@ -0,0 +1,25 @@
+// Copyright 2017 The IriFrance Audio Authors. All rights reserved.  Use of
+// this source code is governed by a license that can be found in the License
+// file.
+
+package ops
+
+import (
+	"zikichombo.org/sound"
+	"zikichombo.org/sound/freq"
+)
+
+type asif struct {
+	sound.Source
+	f freq.T
+}
+
+// AsIf reinterprets a source s as if it were sampled at frequency f.
+func AsIf(s sound.Source, f freq.T) sound.Source {
+	return &asif{Source: s, f: f}
+}
+
+// Freq returns the frequency from the constructor AsIf().
+func (a *asif) Freq() freq.T {
+	return a.f
+}
