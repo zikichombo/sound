f73494d67c3e203c1a0de6b07cfa6570b599ec92
Scott <wsc@iri-labs.com>
2018-08-10 18:32:06 +0200

    typo fix from xmikus01

--- gen/osc_test.go
+++ gen/osc_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The ZikiChomgo Authors. All rights reserved.  Use of this source
+// Copyright 2018 The ZikiChombo Authors. All rights reserved.  Use of this source
 // code is governed by a license that can be found in the License file.
 
 package gen
