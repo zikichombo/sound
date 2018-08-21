8952f74005a3d43439c2c37d3333896643e12cf3
Scott <wsc@iri-labs.com>
2018-08-21 10:07:41 +0200

    fix package doc (thx mewmew)

--- sndbuf/doc.go
+++ sndbuf/doc.go
@@ -1,7 +1,7 @@
 // Copyright 2018 The ZikiChombo Authors. All rights reserved.  Use of this source
 // code is governed by a license that can be found in the License file.
 
-// Package buf provides in memory audio sources and sinks.
+// Package sndbuf provides in memory audio sources and sinks.
 //
 // Buffers are always channel interleaved.
 package sndbuf /* import "zikichombo.org/sound/sndbuf" */
