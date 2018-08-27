1a2e9c76a5454734c37e58025a4756dfd4c0895c
Scott <wsc@iri-labs.com>
2018-08-27 16:48:46 +0200

    add compact cil function
    compaction takes a channel interleaved slice of N frames
    and moves them in place to M <= N frames.
    
    Seems support here is good, as implementing sound.Source
    will likely often require this functionality.

---
+++ cil/compact.go
@@ -0,0 +1,30 @@
+package cil
+
+import "zikichombo.org/sound"
+
+// Compact takes a channel de-interleaved slice d with nC channels
+// and compacts d in place so that it has nF frames placed channel
+// de-interleaved in d[:nF*nC].
+//
+// If nF >= len(d)/nC, then Compact will panic.
+// If len(d)%nC != 0, then Compact will return sound.ErrChannelAlignment.
+func Compact(d []float64, nC, nF int) error {
+	if nC == 1 {
+		return nil
+	}
+	if len(d)%nC != 0 {
+		return sound.ErrChannelAlignment
+	}
+	dF := len(d) / nC
+	if dF == nF {
+		return nil
+	}
+	for c := 0; c < nC; c++ {
+		sStart := c * dF
+		sEnd := sStart + dF
+		dStart := c * nF
+		dEnd := dStart + nF
+		copy(d[dStart:dEnd], d[sStart:sEnd])
+	}
+	return nil
+}
---
+++ cil/compact_test.go
@@ -0,0 +1,25 @@
+package cil
+
+import "testing"
+
+func TestCompact(t *testing.T) {
+	N := 10
+	M := 7
+	C := 3
+	d := make([]float64, N*C)
+	for c := 0; c < C; c++ {
+		for i := 0; i < M; i++ {
+			d[c*N+i] = float64(c*N + i)
+		}
+	}
+	if err := Compact(d, C, M); err != nil {
+		t.Fatal(err)
+	}
+	for c := 0; c < C; c++ {
+		for i := 0; i < M; i++ {
+			if d[c*M+i] != float64(c*N+i) {
+				t.Errorf("got %f not %f\n", d[c*M+i], float64(c*N+i))
+			}
+		}
+	}
+}
