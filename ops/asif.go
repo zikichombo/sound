634a412fc1e6b291cfd25b0a1ea3cbd9e6d72e7f
Scott <wsc@iri-labs.com>
2018-09-19 22:10:12 +0200

    some missing/old license headers

--- ops/asif.go
+++ ops/asif.go
@@ -1,6 +1,5 @@
-// Copyright 2017 The IriFrance Audio Authors. All rights reserved.  Use of
-// this source code is governed by a license that can be found in the License
-// file.
+// Copyright 2018 The ZikiChombo Authors. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
 
 package ops
 
